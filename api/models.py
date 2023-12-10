de97b54c47a04182703d72291734682f7a48349d
Fazliddin Baxtiyorovich <arolboyevfazliddin956@example.com>
2023-12-10 13:28:25 +0500

    Adding: All models.py

--- api/models.py
+++ api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+
 # Create your models here.
+
+class RoomBookingModels(models.Model):
+    rooms = models.TextField(max_length=2000)
+    date = models.DateField()
+
+
+class RoomModels(models.Model):
+    ROOM_CHOICES = (
+        ('Facebook', 'Facebook'),
+        ('Google', 'Google'),
+        ('Amazon', 'Amazon'),
+        ('Netflix ', 'Netflix '),
+        ('Hulu', 'Hulu'),
+        ('McDonalds', 'McDonalds'),
+        ('Halloween', 'Halloween'),
+        ('New York', 'New York'),
+    )
+    message = models.CharField(max_length=2222, default='Xona Band Qilindi!', editable=False)
+    room = models.CharField(max_length=2222, choices=ROOM_CHOICES, blank=True)
+    start = models.TimeField(max_length=2222, blank=True)
+    end = models.TimeField(max_length=2222, blank=True)
+    available_from = models.CharField(max_length=2222, blank=True)
+    is_booked = models.BooleanField(default=False, blank=True)
