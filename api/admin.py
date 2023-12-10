dab7161dde4a0fe51b1959e3f737669a11313e30
Fazliddin Baxtiyorovich <arolboyevfazliddin956@example.com>
2023-12-10 13:30:30 +0500

    Register: admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import RoomBookingModels, RoomModels
 # Register your models here.
+admin.site.register(RoomBookingModels)
+admin.site.register(RoomModels)
+
