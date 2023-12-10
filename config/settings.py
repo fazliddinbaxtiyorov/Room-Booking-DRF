986e3f404d5b6b3dcb102ea9cd731039f4a9f495
Fazliddin Baxtiyorov <137509986+fazliddinbaxtiyorov@users.noreply.github.com>
2023-12-10 17:47:17 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'configver.wsgi.app'
+WSGI_APPLICATION = 'config.wsgi.app'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
