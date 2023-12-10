8879c5bbad72184fe2984c3451bf1768242f8162
Fazliddin Baxtiyorov <137509986+fazliddinbaxtiyorov@users.noreply.github.com>
2023-12-10 17:36:05 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'configver.wsgi.app'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'verceldb',
+        'USER': "default",
+        'HOST': "ep-dark-meadow-07058978-pooler.us-east-1.postgres.vercel-storage.com",
+        'PASSWORD': "lrgGWk8x3wms"
+
     }
 }
 REST_FRAMEWORK = {
