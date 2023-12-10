3b9208f42a8547322f0d12d1e19742687b440d24
Fazliddin Baxtiyorovich <arolboyevfazliddin956@example.com>
2023-12-10 13:51:50 +0500

    Adding: Views Tests

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
+
 
 # Create your tests here.
+class RoomTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_get_all_rooms(self):
+        response = self.client.get('/api/rooms/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def testing_book_rooms(self):
+        response = self.client.get('/api/book/1')
+        self.assertEqual(response.status_code, status.HTTP_301_MOVED_PERMANENTLY)
+
+    def test_website(self):
+        self.client = APIClient()
+        response = self.client.get('hello/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def testing_search_rooms(self):
+        response = self.client.get('/api/search/')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
